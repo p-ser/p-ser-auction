cfa1de3ca09704b4f2d83eb170c81f3ffbac9a4b
amanecopse <amanecopse@gmail.com>
2024-05-07 10:15:52 +0900

    feat: 컨슈머 로직 작성

--- src/main/java/com/pser/auction/consumer/DepositConfirmedConsumer.java
+++ src/main/java/com/pser/auction/consumer/DepositConfirmedConsumer.java
@@ -1,13 +1,38 @@
 package com.pser.auction.consumer;
 
-import com.pser.auction.application.AuctionService;
+import com.pser.auction.application.DepositService;
+import com.pser.auction.config.kafka.KafkaTopics;
+import com.pser.auction.dto.PaymentDto;
+import com.pser.auction.dto.PaymentDto.Response;
+import com.pser.auction.dto.RefundDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class DepositConfirmedConsumer {
-    private final AuctionService auctionService;
+    private final DepositService depositService;
+
+    @RetryableTopic(kafkaTemplate = "paymentDtoValueKafkaTemplate", attempts = "5")
+    @KafkaListener(topics = KafkaTopics.DEPOSIT_CONFIRMED, groupId = "${kafka.consumer-group-id}", containerFactory = "paymentDtoValueListenerContainerFactory")
+    public void updateToConfirmed(PaymentDto paymentDto) {
+        depositService.updateToConfirmed(paymentDto);
+    }
+
+    @DltHandler
+    public void dltHandler(ConsumerRecord<String, PaymentDto> record) {
+        PaymentDto paymentDto = record.value();
+        Response response = paymentDto.getResponse();
+        RefundDto refundDto = RefundDto.builder()
+                .impUid(response.getImpUid())
+                .merchantUid(response.getMerchantUid())
+                .build();
+        depositService.updateToRefundAwaiting(refundDto);
+    }
 }
--- src/main/java/com/pser/auction/consumer/DepositRefundedConsumer.java
+++ src/main/java/com/pser/auction/consumer/DepositRefundedConsumer.java
@@ -1,13 +1,23 @@
 package com.pser.auction.consumer;
 
-import com.pser.auction.application.AuctionService;
+import com.pser.auction.application.DepositService;
+import com.pser.auction.config.kafka.KafkaTopics;
+import com.pser.auction.dto.PaymentDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class DepositRefundedConsumer {
-    private final AuctionService auctionService;
+    private final DepositService depositService;
+
+    @RetryableTopic(kafkaTemplate = "paymentDtoValueKafkaTemplate", attempts = "5")
+    @KafkaListener(topics = KafkaTopics.DEPOSIT_REFUNDED, groupId = "${kafka.consumer-group-id}", containerFactory = "paymentDtoValueListenerContainerFactory")
+    public void updateToRefunded(PaymentDto paymentDto) {
+        depositService.updateToRefunded(paymentDto);
+    }
 }
