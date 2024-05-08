8f5ccafda7321a5a72188bec70985e2eea134602
amanecopse <amanecopse@gmail.com>
2024-05-08 09:58:04 +0900

    refactor: Kafka topic 이름 변경

--- src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
+++ src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
@@ -1,8 +1,8 @@
 package com.pser.auction.config.kafka;
 
 public interface KafkaTopics {
-    String DEPOSIT_CONFIRM_AWAITING = "deposit-confirm-awaiting";
-    String DEPOSIT_REFUND_AWAITING = "deposit-refund-awaiting";
-    String DEPOSIT_CONFIRMED = "deposit-confirmed";
-    String DEPOSIT_REFUNDED = "deposit-refunded";
+    String DEPOSIT_CONFIRM_AWAITING = "deposit.confirm-awaiting";
+    String DEPOSIT_REFUND_AWAITING = "deposit.refund-awaiting";
+    String DEPOSIT_CONFIRMED = "deposit.confirmed";
+    String DEPOSIT_REFUNDED = "deposit.refunded";
 }
