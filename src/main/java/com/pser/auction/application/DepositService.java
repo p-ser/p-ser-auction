95b1ddb9e7f86b01b14299063854d93a13ae2a25
amanecopse <amanecopse@gmail.com>
2024-05-08 15:52:20 +0900

    [KSP-166] feat: Deposit Service 구현

--- src/main/java/com/pser/auction/application/DepositService.java
+++ src/main/java/com/pser/auction/application/DepositService.java
@@ -1,41 +1,71 @@
 package com.pser.auction.application;
 
+import com.pser.auction.dao.AuctionDao;
+import com.pser.auction.dao.DepositDao;
+import com.pser.auction.domain.Auction;
+import com.pser.auction.domain.Deposit;
+import com.pser.auction.domain.DepositStatusEnum;
 import com.pser.auction.dto.ConfirmDto;
+import com.pser.auction.dto.DepositCreateRequest;
+import com.pser.auction.dto.DepositMapper;
 import com.pser.auction.dto.PaymentDto;
+import com.pser.auction.dto.PaymentDto.Response;
 import com.pser.auction.dto.RefundDto;
 import com.pser.auction.producer.DepositConfirmAwaitingProducer;
 import com.pser.auction.producer.DepositRefundAwaitingProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DepositService {
+    private final DepositMapper depositMapper;
+    private final AuctionDao auctionDao;
+    private final DepositDao depositDao;
     private final DepositConfirmAwaitingProducer depositConfirmAwaitingProducer;
     private final DepositRefundAwaitingProducer depositRefundAwaitingProducer;
 
-    public long save() {
-        // TODO: 결제 대기 상태 보증금 생성
-        return 1L;
+    @Transactional
+    public long save(DepositCreateRequest request) {
+        Auction auction = auctionDao.findById(request.getAuctionId())
+                .orElseThrow();
+        Deposit deposit = depositMapper.toEntity(request, auction);
+        depositDao.save(deposit);
+        return deposit.getId();
     }
 
+    @Transactional
     public void updateToConfirmAwaiting(ConfirmDto confirmDto) {
-        // TODO: 상태를 컨펌 대기 상태로 변경
+        Deposit deposit = depositDao.findByMerchantUid(confirmDto.getMerchantUid())
+                .orElseThrow();
+        deposit.updateStatus(DepositStatusEnum.CONFIRM_AWAITING);
         depositConfirmAwaitingProducer.notifyConfirmAwaiting(confirmDto);
     }
 
+    @Transactional
     public void updateToRefundAwaiting(RefundDto refundDto) {
-        // TODO: 상태를 환불 대기 상태로 변경
+        Deposit deposit = depositDao.findByMerchantUid(refundDto.getMerchantUid())
+                .orElseThrow();
+        deposit.updateStatus(DepositStatusEnum.REFUND_AWAITING);
         depositRefundAwaitingProducer.notifyRefundAwaiting(refundDto);
     }
 
+    @Transactional
     public void updateToConfirmed(PaymentDto paymentDto) {
-        // TODO: 상태를 컨펌 완료 상태로 변경
+        Response response = paymentDto.getResponse();
+        Deposit deposit = depositDao.findByMerchantUid(response.getMerchantUid())
+                .orElseThrow();
+        deposit.updateStatus(DepositStatusEnum.CONFIRMED);
     }
 
+    @Transactional
     public void updateToRefunded(PaymentDto paymentDto) {
-        // TODO: 상태를 환불 완료 상태로 변경
+        Response response = paymentDto.getResponse();
+        Deposit deposit = depositDao.findByMerchantUid(response.getMerchantUid())
+                .orElseThrow();
+        deposit.updateStatus(DepositStatusEnum.REFUNDED);
     }
 }
