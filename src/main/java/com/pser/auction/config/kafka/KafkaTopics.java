fe37c9ac64c8b545db3f41ef759af42b4c076b9e
amanecopse <amanecopse@gmail.com>
2024-05-13 14:13:37 +0900

    feat: DepositConfirmAwaitingRollbackConsumer 추가

--- src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
+++ src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
@@ -3,6 +3,7 @@ package com.pser.auction.config.kafka;
 public interface KafkaTopics {
     String DEPOSIT_PAYMENT_AWAITING = "deposit.payment-awaiting";
     String DEPOSIT_CONFIRM_AWAITING = "deposit.confirm-awaiting";
+    String DEPOSIT_CONFIRM_AWAITING_ROLLBACK = "deposit.confirm-awaiting-rollback";
     String DEPOSIT_REFUND_AWAITING = "deposit.refund-awaiting";
     String DEPOSIT_CONFIRMED = "deposit.confirmed";
     String DEPOSIT_REFUNDED = "deposit.refunded";
---
+++ src/main/java/com/pser/auction/infra/kafka/consumer/DepositConfirmAwaitingRollbackConsumer.java
@@ -0,0 +1,22 @@
+package com.pser.auction.infra.kafka.consumer;
+
+import com.pser.auction.application.DepositService;
+import com.pser.auction.config.kafka.KafkaTopics;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DepositConfirmAwaitingRollbackConsumer {
+    private final DepositService depositService;
+
+    @RetryableTopic(kafkaTemplate = "stringValueKafkaTemplate", attempts = "5")
+    @KafkaListener(topics = KafkaTopics.DEPOSIT_CONFIRM_AWAITING_ROLLBACK, groupId = "${kafka.consumer-group-id}", containerFactory = "stringValueListenerContainerFactory")
+    public void scheduleJob(String merchantUid) {
+        depositService.rollbackToPaymentAwaiting(merchantUid);
+    }
+}
