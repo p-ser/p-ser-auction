f75a64430b557632be408b44872e02dd9e7fe286
amanecopse <amanecopse@gmail.com>
2024-05-13 14:03:31 +0900

    feat: DepositDao 조회 함수 종류 추가

--- src/main/java/com/pser/auction/dao/DepositDao.java
+++ src/main/java/com/pser/auction/dao/DepositDao.java
@@ -1,9 +1,19 @@
 package com.pser.auction.dao;
 
 import com.pser.auction.domain.Deposit;
+import com.pser.auction.domain.DepositStatusEnum;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 public interface DepositDao extends JpaRepository<Deposit, Long> {
     Optional<Deposit> findByMerchantUid(String merchantUid);
+
+    Optional<Deposit> findByUserIdAndAuctionIdAndStatusIn(Long userId, Long auctionId, List<DepositStatusEnum> statusEnums);
+
+    @NonNull
+    Page<Deposit> findAll(@NonNull Pageable pageable);
 }
