697ad10f9e1aadcf56d809b51a806cc071e5fb2f
amanecopse <amanecopse@gmail.com>
2024-05-16 16:49:13 +0900

    refactor: AuctionRestApi 수정

--- src/main/java/com/pser/auction/api/AuctionRestApi.java
+++ src/main/java/com/pser/auction/api/AuctionRestApi.java
@@ -1,5 +1,6 @@
 package com.pser.auction.api;
 
+import com.pser.auction.application.AuctionService;
 import com.pser.auction.common.response.ApiResponse;
 import com.pser.auction.dto.AuctionCreateRequest;
 import com.pser.auction.dto.AuctionResponse;
@@ -10,8 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,33 +25,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auctions")
 @RequiredArgsConstructor
 public class AuctionRestApi {
+    private final AuctionService auctionService;
+
     @GetMapping("/search")
     public ResponseEntity<ApiResponse<Page<AuctionResponse>>> search(AuctionSearchRequest request,
                                                                      @PageableDefault Pageable pageable) {
-        //TODO: 서비스, DAO 구현 필요
+        //TODO: 서비스 구현 필요, Elasticsearch 로 구현할 것
         Page<AuctionResponse> result = null;
         return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @GetMapping("/{auctionId}")
     public ResponseEntity<ApiResponse<AuctionResponse>> getById(@PathVariable long auctionId) {
-        //TODO: 서비스, DAO 구현 필요
+        //TODO: 서비스, Elasticsearch 에서 가져온 Materialized View 를 반환
         AuctionResponse result = null;
         return ResponseEntity.ok(ApiResponse.success(result));
     }
 
     @PostMapping
-    //TODO: Preauthorize Custom Method 를 구현하여 경매의 원래 예약자가 인증 유저와 일치하는 지 검증할 것
+    @PreAuthorize("@hotelClient.isReservationOwner(request.reservationId, authId)")
     public ResponseEntity<Void> save(@RequestHeader("User-Id") long authId,
                                      @Validated @RequestBody AuctionCreateRequest request) {
         //TODO: 서비스, DAO 구현 필요
-        long id = 1L;
+        request.setAuthId(authId);
+        long id = auctionService.save(request);
         return ResponseEntity.created(URI.create("/auctions/" + id)).build();
-    }
-
-    @DeleteMapping("/{auctionId}")
-    //TODO: Preauthorize Custom Method 를 구현하여 경매의 원래 예약자가 인증 유저와 일치하는 지 검증할 것
-    public ResponseEntity<Void> delete(@RequestHeader("User-Id") long authId, @PathVariable long auctionId) {
-        return ResponseEntity.noContent().build();
     }
 }
