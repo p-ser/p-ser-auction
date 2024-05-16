d41abc0a7637718f965ad5e799420f0f1c49caad
amanecopse <amanecopse@gmail.com>
2024-05-16 16:50:06 +0900

    refactor: AuctionCreateRequest 수정

--- src/main/java/com/pser/auction/dto/AuctionCreateRequest.java
+++ src/main/java/com/pser/auction/dto/AuctionCreateRequest.java
@@ -1,7 +1,9 @@
 package com.pser.auction.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,14 +14,19 @@ import lombok.Data;
 @AllArgsConstructor
 public class AuctionCreateRequest {
     @NotNull
-    private long auctionedReservationId;
+    private long reservationId;
+
+    @Schema(hidden = true)
+    @Null
+    private Long authId;
+
+    @NotNull
+    @Min(0)
+    private int endPrice;
 
     @NotNull
     @Min(0)
     private int price;
-
-    @NotNull
-    private LocalDateTime startAt;
 
     @NotNull
     private LocalDateTime endAt;
