602e61caf11a0fa646f381dd19f944ac1d45d8dd
amanecopse <amanecopse@gmail.com>
2024-05-20 10:47:25 +0900

    feat: AuctionStatusEnum 에서 StatusEnum 상속

--- src/main/java/com/pser/auction/domain/AuctionStatusEnum.java
+++ src/main/java/com/pser/auction/domain/AuctionStatusEnum.java
@@ -1,6 +1,8 @@
 package com.pser.auction.domain;
 
+import com.pser.auction.domain.event.StatusEnum;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,13 +10,43 @@ import java.util.stream.Stream;
 import lombok.Getter;
 
 @Getter
-public enum AuctionStatusEnum {
-    CREATED(0),
-    ON_GOING(1),
-    PAYMENT_AWAITING(2),
-    PAID(3),
-    PAYMENT_DENIAL(4),
-    FAILURE(5);
+public enum AuctionStatusEnum implements StatusEnum {
+    CREATED(0) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return List.of(ONGOING);
+        }
+    },
+    ONGOING(1) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return List.of(PAYMENT_AWAITING, FAILURE);
+        }
+    },
+    PAYMENT_AWAITING(2) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return List.of(PAID, PAYMENT_DENIAL);
+        }
+    },
+    PAID(3) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return null;
+        }
+    },
+    PAYMENT_DENIAL(4) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return null;
+        }
+    },
+    FAILURE(5) {
+        @Override
+        public List<StatusEnum> getNext() {
+            return null;
+        }
+    };
 
     private static final Map<Integer, AuctionStatusEnum> valueToName =
             Collections.unmodifiableMap(Stream.of(values())
