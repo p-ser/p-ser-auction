af6407230270c004d5c7e43fb018c2d3d54b56c8
amanecopse <amanecopse@gmail.com>
2024-05-20 10:43:26 +0900

    feat: 경매 관련 카프카 토픽 추가

--- src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
+++ src/main/java/com/pser/auction/config/kafka/KafkaTopics.java
@@ -2,6 +2,9 @@ package com.pser.auction.config.kafka;
 
 public interface KafkaTopics {
     String AUCTION_CREATED = "auction.created";
+    String AUCTION_CREATED_ROLLBACK = "auction.created-rollback";
+    String AUCTION_PAYMENT_AWAITING = "auction.payment-awaiting";
+    String AUCTION_FAILURE = "auction.failure";
     String DEPOSIT_CREATED = "deposit.created";
     String DEPOSIT_CONFIRM_AWAITING = "deposit.confirm-awaiting";
     String DEPOSIT_CONFIRM_AWAITING_ROLLBACK = "deposit.confirm-awaiting-rollback";
